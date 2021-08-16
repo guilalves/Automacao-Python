93790d633af560951de4e16852f8423d97df1d32
guilherme <guilherme.alves96@hotmail.com>
2021-08-16 16:32:34 -0300

    Automatizando o envio de emails pelo gmail  automatico

---
+++ gmail/config.py
@@ -0,0 +1,30 @@
+import email.message
+import emoji
+import smtplib
+
+
+def config_email():
+
+    emojis = emoji.emojize(':v: :wink:', use_aliases=True)
+
+    corpo_email = f"""
+    <p>Testando inteligência artifical para envio de emails automático feito em Python</p>
+    <p>Abs,</p>
+    <p>Guilherme {emojis}</p>
+    """
+
+     # Configurações do email
+    msg = email.message.Message()
+    msg['Subject'] = "E-mail Automático"
+    msg['From'] = 'seugmail@gmail.com'
+    msg['To'] = 'emailparaquemvocequerenviar@gmail.com'
+    password = 'suasenhadogmail'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+
+    # Credenciais de login para envio do email
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
